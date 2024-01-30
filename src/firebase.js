a7be6bac2066270cbb553cb67d05dc86e8f04e49
joanm <joan.marquenyo@gmail.com>
2024-01-30 14:14:24 +0100

    first

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, query, orderBy, getDocs } from 'firebase/firestore/lite';
-import dotenv from 'dotenv';
-dotenv.config();
-
-// Load environment variables from .env file
 
 
 
 const firebaseConfig = {
-    apiKey: import.meta.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: import.meta.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    projectId: import.meta.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-    appId: import.meta.env.REACT_APP_FIREBASE_APPID
+    apiKey : import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain : import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+    projectId : import.meta.env.VITE_FIREBASE_PROJECTID,
+    storageBucket : import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+    messagingSenderId : import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+    appId : import.meta.env.VITE_FIREBASE_APPID,
 };
-
-
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
